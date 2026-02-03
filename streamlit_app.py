fa121cdc819b48b2b0e57b024a917291b63d5502
Bjarne Herbst <bjarneh22@gmail.com>
2026-02-03 18:25:32 +0100

    collector changes and new streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+import pandas as pd 
+import time 
+import sys 
+import os 
+
+# src Verzeichnis zum Pfad hinzufügen
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
